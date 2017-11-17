05c5db4cafcf41bc1b424fd80dd703e9e38279da
Cezary Olejarczyk <colejarczyk@divante.pl>
2017-11-17 11:51:33 +0100

    Build & push docker images to DockerHub.

---
+++ frontend/src/config.js
@@ -0,0 +1,16 @@
+const config = {
+    "apiUrl": "http://openloyalty.localhost/api",
+    "dateFormat": "YYYY-MM-DD",
+    "dateTimeFormat": "YYYY-MM-DD HH:mm",
+    "perPage": 20,
+    "debug": false,
+    "modules": []
+};
+window.OpenLoyaltyConfig = {
+    "apiUrl": "http://openloyalty.localhost/api",
+    "dateFormat": "YYYY-MM-DD",
+    "dateTimeFormat": "YYYY-MM-DD HH:mm",
+    "perPage": 20,
+    "debug": false,
+    "modules": []
+};
